22c27ef69c6803fb326e8ea337bea725c20618dc
Rares-Daniel <rares-daniel.constantin@student.tuiasi.ro>
2025-05-10 10:21:41 +0300

    GoogleTTSDoc

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ro.tuiasi.ac.ProiectPIP","l":"ChatService"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ro.tuiasi.ac.ProiectPIP","l":"GoogleTTS"}];updateSearchResults();
--- src/main/java/ro/tuiasi/ac/ProiectPIP/GoogleTTS.java
+++ src/main/java/ro/tuiasi/ac/ProiectPIP/GoogleTTS.java
@@ -178,7 +178,7 @@ public class GoogleTTS {
     }
 }
 */
-
+/*
 package ro.tuiasi.ac.ProiectPIP;
 
 import com.google.cloud.texttospeech.v1.*;
@@ -259,4 +259,125 @@ public class GoogleTTS {
         return text;
     }
 
+}
+*/
+
+package ro.tuiasi.ac.ProiectPIP;
+
+import com.google.cloud.texttospeech.v1.*;
+import com.google.protobuf.ByteString;
+
+import javax.sound.sampled.*;
+import java.io.*;
+
+/**
+ * {@code GoogleTTS} este responsabil pentru sintetizarea textului in voce
+ * folosind Google Cloud Text-to-Speech API si redarea acestuia prin difuzoare.
+ *
+ * <p>
+ * Suporta limba romana si permite oprirea redarii in timpul functionarii.
+ * </p>
+ *
+ * <p><b>Exemplu de utilizare:</b></p>
+ * <pre>{@code
+ * GoogleTTS tts = new GoogleTTS();
+ * tts.speak("Salut, cum pot sa te ajut?");
+ * }</pre>
+ *
+ * @author Marian
+ * @version 1.0
+ * @see com.google.cloud.texttospeech.v1.TextToSpeechClient
+ * @see javax.sound.sampled.SourceDataLine
+ */
+public class GoogleTTS {
+
+    private volatile SourceDataLine currentLine;
+
+    /**
+     * Converteste un text in voce folosind Google Cloud Text-to-Speech
+     * si reda rezultatul audio prin difuzoare.
+     *
+     * @param text textul care va fi sintetizat si redat
+     * @throws Exception daca sintetizarea sau redarea esueaza
+     * @see #stop()
+     */
+    public void speak(String text) throws Exception {
+        text = cleanText(text);
+
+        try (TextToSpeechClient ttsClient = TextToSpeechClient.create()) {
+            SynthesisInput input = SynthesisInput.newBuilder()
+                    .setText(text)
+                    .build();
+
+            VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+                    .setLanguageCode("ro-RO")
+                    .setName("ro-RO-Wavenet-A")
+                    .build();
+
+            AudioConfig audioConfig = AudioConfig.newBuilder()
+                    .setAudioEncoding(AudioEncoding.LINEAR16)
+                    .setSpeakingRate(1.3f)
+                    .build();
+
+            SynthesizeSpeechResponse response = ttsClient.synthesizeSpeech(input, voice, audioConfig);
+            byte[] audioData = response.getAudioContent().toByteArray();
+
+            AudioFormat format = new AudioFormat(16000, 16, 1, true, false);
+            try (SourceDataLine line = AudioSystem.getSourceDataLine(format)) {
+                line.open(format);
+                line.start();
+                this.currentLine = line;
+
+                line.write(audioData, 0, audioData.length);
+
+                line.drain();
+                line.stop();
+                this.currentLine = null;
+            }
+
+            System.out.println("Redarea audio s-a incheiat.");
+        } catch (Exception e) {
+            System.err.println("Eroare la redare: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    /**
+     * Opreste redarea vocii daca aceasta este in curs.
+     * Folosit pentru a permite utilizatorului sa intrerupa vocea instant.
+     *
+     * @see #speak(String)
+     */
+    public synchronized void stop() {
+        try {
+            if (currentLine != null) {
+                if (currentLine.isRunning()) {
+                    currentLine.stop();
+                }
+                currentLine.flush();
+                currentLine.close();
+                System.out.println("Redarea vocala a fost oprita de utilizator.");
+            } else {
+                System.out.println("Redarea era deja oprita sau nu fusese pornita.");
+            }
+        } catch (Exception e) {
+            System.err.println("Eroare la oprirea redarii: " + e.getMessage());
+        } finally {
+            currentLine = null;
+        }
+    }
+
+    /**
+     * Elimina formatarea Markdown si tag-urile speciale din text,
+     * pentru a-l pregati pentru sintetizare.
+     *
+     * @param text textul brut care poate contine formatare
+     * @return textul curatat pentru a fi redat vocal
+     */
+    protected String cleanText(String text) {
+        if (text == null) return "";
+        text = text.replaceAll("\\*\\*(.*?)\\*\\*", "$1");
+        text = text.replaceAll("###.*?###\\s*", "");
+        return text;
+    }
 }
