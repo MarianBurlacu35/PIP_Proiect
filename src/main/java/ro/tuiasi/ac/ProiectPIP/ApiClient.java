9d90e3128df2c424d5a845ad31563359bff6b146
Raul_Ghergheluc <raul.ghergheluc@student.tuiasi.ro>
2025-05-12 08:48:29 +0300

    Eliminare

--- src/main/java/ro/tuiasi/ac/ProiectPIP/ApiClient.java
+++ src/main/java/ro/tuiasi/ac/ProiectPIP/ApiClient.java
@@ -1,21 +1,3 @@
-/*
-
-package ro.tuiasi.ac.ProiectPIP;
-
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.OkHttpClient;
-
-public class ApiClient {
-    public static OkHttpClient getClient() {
-        return new OkHttpClient.Builder()
-                .connectTimeout(60, TimeUnit.SECONDS)
-                .readTimeout(60, TimeUnit.SECONDS)
-                .writeTimeout(60, TimeUnit.SECONDS)
-                .build();
-    }
-}
-*/
 package ro.tuiasi.ac.ProiectPIP;
 
 import java.util.concurrent.TimeUnit;
